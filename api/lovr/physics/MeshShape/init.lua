5e77a2116c028befd8a736fb968df74a3184e8ed
xiejiangzhi <xiejiangzhi@gmail.com>
2024-12-27 10:10:50 +0800

    Add note to MeshShape

--- api/lovr/physics/MeshShape/init.lua
+++ api/lovr/physics/MeshShape/init.lua
@@ -2,6 +2,9 @@ return {
   summary = 'A mesh Shape.',
   description = 'A type of `Shape` that can be used for triangle meshes.',
   extends = 'Shape',
+  notes = [[
+    MeshShape is static. If the collider contains a MeshShape, it will be forced to become static.
+  ]],
   constructors = {
     'lovr.physics.newMeshShape',
     'World:newMeshCollider'
